7e4535912d3d6e01e61f887f6a2252f50060d475
s110519007 <s110519007@stu.ntue.edu.tw>
2023-10-05 14:53:56 +0800

    Add files via upload

---
+++ data-elevator.js
@@ -0,0 +1,29 @@
+/*
+ ** TODO: 電梯DATA ******************
+ */
+// 小網下方固定按鈕圖片位置(left: 左圖右文 top: 上圖下文)
+var iconPosition = "left";
+
+// 小網下方固定按鈕(文案、連結;請不要超過五組)
+var iconName = [
+  {
+    "linkUrl": "https://media.etmall.com.tw/sp/9475/m/index.html",
+    "imageUrl": "icon-1.png",
+    "words": "B滿額贈",
+  },
+  {
+    "linkUrl": "",
+    "imageUrl": "icon-2.png",
+    "words": "A高額",
+  },
+  {
+    "linkUrl": "https://media.etmall.com.tw/sp/9475/m/index.html",
+    "imageUrl": "icon-3.png",
+    "words": "B滿額贈",
+  },
+  // {
+  //     "linkUrl": "",
+  //     "imageUrl": "",
+  //     "words": "",
+  // },
+]
